528ffccc777a721ea2c5b46cab92861a56ff934b
Neojan <neojanlocke@gmail.com>
2019-08-25 22:29:46 +0800

    添加项目文件。

---
+++ codeGenerator/codeGeneratorDlg.h
@@ -0,0 +1,34 @@
+
+// codeGeneratorDlg.h : header file
+//
+
+#pragma once
+
+
+// CcodeGeneratorDlg dialog
+class CcodeGeneratorDlg : public CDialogEx
+{
+// Construction
+public:
+	CcodeGeneratorDlg(CWnd* pParent = NULL);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_CODEGENERATOR_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
